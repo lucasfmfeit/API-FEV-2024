f505b52e6d7e84b0ebb2107af1f947a2e1a2d2fd
Lucas Feitosa <lucasfmfeit@gmail.com>
2024-03-08 07:57:58 -0300

    alteramos o put

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const express = require("express");
 const app = express();
-const data = [
+let data = [
     {
         nome:"Ronaldo",
         pais:"Brasil",
-        posicao:"Atacante"
+        posicao:"Atacante",
+        id:1,
     },
     {
         nome:"Kaká",
         pais:"Brasil",
-        posicao:"Meio Campo"
+        posicao:"Meio Campo",
+        id:2
     },
     {
         nome:"Buffon",
         pais:"Italia",
-        posicao:"Goleiro"
+        posicao:"Goleiro",
+        id:3
     }
+
 ]
+app.use(express.json())
 
 app.get("/players",(request,response)=>{
+    return response.json(data)
+})
+
+app.post("/players",(request, response)=>{
+    const body = request.body
+    const ultimo = data[data.length - 1]
+    const id = ultimo.id + 1
+    data.push({
+        ...body, id
+    })
+    return response.json(data)
+})
+
+app.put("/players/:id",(request,response)=>{
+    const id = request.params?.id
+    const body = request.body
+    data = data.map((item)=>{
+        if(item.id===Number(id)){
+            return {
+                ...item,...body
+            }
+        }
+        return item;
+    })
     return response.json(data)
 })
 
 app.listen(3000,()=>{
     console.log("Servidor rodando!")
 })
+
